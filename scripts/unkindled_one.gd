1e5ba3ffdd4de4e87de7d80c2284680a7993c018
SuburbanThug613 <sami.al-khany@stu.ocsb.ca>
2025-06-05 13:19:36 -0400

    Add files via upload
    second level beta

--- scripts/unkindled_one.gd
+++ scripts/unkindled_one.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var animated_sprite = $AnimatedSprite2D
+
 const SPEED = 300.0
 const JUMP_VELOCITY = -450.0
 const GRAVITY = 1200.0
@@ -105,6 +107,25 @@ func _physics_process(delta: float) -> void:
 		attack()
 
 	move_and_slide()
+	handle_movement_animation(facing_direction)
+
+# In the function definition
+func handle_movement_animation(dir):
+	if is_on_floor():
+		if !velocity:
+			animated_sprite.play("idle")
+		if velocity:
+			animated_sprite.play("run")
+			toggle_flip_sprite(dir)
+	elif !is_on_floor():
+		animated_sprite.play("fall")
+
+# Flip sprite based on direction
+func toggle_flip_sprite(dir):
+	if dir == 1:
+		animated_sprite.flip_h = false
+	if dir == -1:
+		animated_sprite.flip_h = true
 
 
 func handle_timers(delta: float) -> void:
